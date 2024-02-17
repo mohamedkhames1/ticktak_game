8d73f680881d379988e111b0477062246d7d4a7a
mohamedkhames1 <moham01555@gmail.com>
2024-02-17 14:13:43 +0200

    add textStyle

--- booklyapp/lib/core/utlis/styles.dart
+++ booklyapp/lib/core/utlis/styles.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
-abstract class Styles{
-  static TextStyle titlemudeim=const TextStyle(fontSize: 18,fontWeight: FontWeight.w600) ;
+abstract class Styles {
+  static TextStyle textStyle18 =
+      const TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+
+  static TextStyle textStyle20 =
+      const TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
+
+  static TextStyle textStyle30 =
+      const TextStyle(fontSize: 30, fontWeight: FontWeight.normal);
+
+  static TextStyle textStyle14 =
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+
+  static TextStyle textStyle16 =
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
 }
--- booklyapp/lib/features/home/presentation/view/ widgets/homeViewBody.dart
+++ booklyapp/lib/features/home/presentation/view/ widgets/homeViewBody.dart
@@ -1,5 +1,7 @@
 import 'package:booklyapp/core/utlis/styles.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utlis/assets.dart';
+import 'customlistviewitems.dart';
 import 'futurbookslistview.dart';
 import 'homeviewAppBar.dart';
 
@@ -19,9 +21,31 @@ class HomeViewBody extends StatelessWidget {
             const CustomAppbar(),
             const FutureBooksListView(),
             const SizedBox(height: 60,),
-            Text('Best Seller ',style: Styles.titlemudeim,),
+            Text('Best Seller ',style: Styles.textStyle18,),
+            const SizedBox(height: 20,),
+            const BestSellerListViewItems()
           ],
         ),
+      ),
+    );
+  }
+}
+class BestSellerListViewItems extends StatelessWidget {
+  const BestSellerListViewItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  const SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          FutureListViewItems(),
+          Column(
+            children: [
+
+            ],
+          )
+        ],
       ),
     );
   }
