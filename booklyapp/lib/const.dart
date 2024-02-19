1b12d7ef9fd477200f423c0eb237a02c237e2e74
mohamedkhames1 <92439267+mohamedkhames1@users.noreply.github.com>
2024-02-19 17:21:35 +0200

    Delete booklyapp directory

--- booklyapp/lib/const.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kprimaryclr = Color(0xff100B20);
-
-const mm = 'assets/img.png';
-const klogo = 'assets/Logo.png';
-
-const ktranstion = Duration(seconds: 1);
-const ksearch='assets/ic search.png';
--- booklyapp/lib/core/utlis/assets.dart
+++
@@ -1,4 +0,0 @@
-class AssetsData {
-  static const logo = 'assets/Logo.png';
-  static const testImage = 'assets/img_1.png';
-}
